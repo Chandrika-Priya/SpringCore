90311cec4aedde3652b23a82ee82a8e351eb2411
ChandrikaPriya <priya.bogadi@thoughtworks.com>
2022-05-16 16:41:08 +0530

    [Priya] ADD. Dependency Injection using java annotations using constructor injection.

--- src/main/java/com/ioc/inversionofControl/sports/Baseball.java
+++ src/main/java/com/ioc/inversionofControl/sports/Baseball.java
@@ -1,12 +1,20 @@
 package com.ioc.inversionofControl.sports;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Baseball implements Coach {
+    private Fortune fortune;
+
+    @Autowired
+    public Baseball(Fortune fortune) {
+        this.fortune = fortune;
+    }
 
     @Override
     public String getDetails() {
+        System.out.println(fortune.getFortune());
         return "Base Ball ";
     }
 }
--- src/main/java/com/ioc/inversionofControl/sports/HappyFortune.java
+++ src/main/java/com/ioc/inversionofControl/sports/HappyFortune.java
@@ -1,6 +1,10 @@
 package com.ioc.inversionofControl.sports;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HappyFortune implements Fortune {
+
     @Override
     public String getFortune() {
         return "Happy Fortune";
