e9a559838e3ba1064d1d4983974c48be2846c481
ChandrikaPriya <priya.bogadi@thoughtworks.com>
2022-05-17 11:51:29 +0530

    [Priya] ADD. Injecting values from properties file using Java Annotations with no xml.

--- src/main/java/com/ioc/inversionofControl/sports/Baseball.java
+++ src/main/java/com/ioc/inversionofControl/sports/Baseball.java
@@ -1,12 +1,18 @@
 package com.ioc.inversionofControl.sports;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("application.properties")
 public class Baseball implements Coach {
 
     private Fortune fortune;
+
+    @Value("${team}")
+    private String team;
 
     public Baseball(@Qualifier("baseball") Fortune fortune) {
         this.fortune = fortune;
@@ -15,6 +21,7 @@ public class Baseball implements Coach {
     @Override
     public String getDetails() {
         System.out.println(fortune.getFortune());
+        System.out.println(team);
         return "Base Ball ";
     }
 }
